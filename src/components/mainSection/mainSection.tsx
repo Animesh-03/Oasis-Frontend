5b0a7416abfe4e80b5ef50f5ac1f9df512ed5cb1
Akshat Khaitan <khaitanakshat40@gmail.com>
2023-02-28 00:26:16 +0530

    Dashboard added

---
+++ src/components/mainSection/mainSection.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import css from "./mainSection.module.css";
+
+function MainSection({ option }) {
+	return (
+		<>
+			<div className={css["outer"]}>
+				Oasis were an English rock band formed in Manchester in 1991. Originally
+				known as the Rain, the group initially consisted of Liam Gallagher (lead
+				vocals, tambourine), Paul Arthurs (guitar), Paul McGuigan (bass guitar)
+				and Tony McCarroll (drums). Liam's older brother Noel (lead guitar,
+				vocals) later joined as a fifth member, finalising the group's core
+				lineup. During the course of their existence, they had various lineup
+				changes, with the Gallagher brothers remaining the only staple members.
+				Oasis signed to independent record label Creation Records in 1993 and
+				released their record-setting debut album Definitely Maybe (1994). The
+				following year they recorded (What's the Story) Morning Glory? (1995)
+				with drummer Alan White, in the midst of a chart rivalry with peers
+				Blur. Spending ten weeks at number one on the UK Albums Chart, (What's
+				the Story) Morning Glory? was also an international chart success and
+				became one of the best-selling albums of all time. In addition, it
+				stands as the fifth-best-selling album in the UK and the biggest-selling
+				album in the UK of the 1990s. The Gallagher brothers featured regularly
+				in tabloid newspapers for their disputes and wild lifestyles. In 1996,
+				Oasis performed two nights at Knebworth for an audience of 125,000 each
+				time, the largest outdoor concerts in UK history at the time. In 1997,
+				Oasis released their third album, Be Here Now. It became the
+				fastest-selling album in UK chart history. Oasis' popularity later
+				declined and McGuigan and Arthurs left in 1999 as Oasis released
+				Standing on the Shoulder of Giants (2000). They were replaced by former
+				Heavy Stereo guitarist Gem Archer and former Ride guitarist Andy Bell.
+				White departed in 2004, replaced by Zak Starkey and later Chris Sharrock
+				as touring members. Oasis released three more studio albums, Heathen
+				Chemistry (2002), Don't Believe the Truth (2005) and Dig Out Your Soul
+				(2008). The group abruptly disbanded after the departure of Noel
+				Gallagher in August 2009. As of 2009, Oasis had sold over 70 million
+				records worldwide.[1][2] They are among the most successful acts on the
+				UK Singles Chart and Albums Chart, with eight UK number-one singles and
+				eight UK number-one albums.[3][4][5] The band also achieved three
+				platinum albums in the US. They won 17 NME Awards, nine Q Awards, four
+				MTV Europe Music Awards and six Brit Awards, including one in 2007 for
+				Outstanding Contribution to Music and one for the "Best Album of the
+				Last 30 Years" for (What's the Story) Morning Glory?. They were
+				nominated for two Grammy Awards.[6] Oasis were an English rock band
+				formed in Manchester in 1991. Originally known as the Rain, the group
+				initially consisted of Liam Gallagher (lead vocals, tambourine), Paul
+				Arthurs (guitar), Paul McGuigan (bass guitar) and Tony McCarroll
+				(drums). Liam's older brother Noel (lead guitar, vocals) later joined as
+				a fifth member, finalising the group's core lineup. During the course of
+				their existence, they had various lineup changes, with the Gallagher
+				brothers remaining the only staple members. Oasis signed to independent
+				record label Creation Records in 1993 and released their record-setting
+				debut album Definitely Maybe (1994). 
+			</div>
+		</>
+	);
+}
+
+export default MainSection;
