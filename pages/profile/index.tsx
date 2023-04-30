cfe162dfc516e80dd0a800c65a5f72bd032a3b82
Vibhum Raj Tripathi <93042212+V1BHUM@users.noreply.github.com>
2023-04-30 16:14:01 +0530

    Update index.tsx

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -6,7 +6,7 @@ interface ProfilePageProps {
 
 }
 
-const ProfilePage: React.FC<ProfilePageProps> = ({}) => {
+const ProfilePage: React.FC<ProfilePageProps> = () => {
 
     return (
         <MainSection>
