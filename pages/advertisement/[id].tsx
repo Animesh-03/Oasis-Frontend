4e395d4377ddf2979b1fc0e8b0f91eecd60865e0
Animesh-03 <anivad03@gmail.com>
2023-05-08 19:25:31 +0530

    revert ads

--- pages/advertisement/[id].tsx
+++ pages/advertisement/[id].tsx
@@ -2,7 +2,7 @@ import React from "react";
 import css from "./advertisement.module.css"
 import MainSection from "@/components/mainSection/mainSection"
 import BookCard from "@/components/book card/bookCard";
-import { useGetAdvertisementQuery, useGetTrendingBooksQuery, useGetBookRatingLazyQuery} from "@/graphql/generated/generated";
+import { useGetAdvertisementQuery, useGetTrendingBooksQuery } from "@/graphql/generated/generated";
 import withApollo from '@/apollo/client';
 import Advert from "@/components/advert/advert";
 import { useRouter } from "next/router";
@@ -11,23 +11,6 @@ function Test() {
     const router = useRouter();
     const { data, loading } = useGetAdvertisementQuery({ variables: { advertisement: router.query.id as string } });
     const { data: trendingData, loading: trendingLoading } = useGetTrendingBooksQuery();
-    const [getBookRating] = useGetBookRatingLazyQuery();
-
-    const [rating, setRating] = React.useState(0);
-    
-    React.useEffect(() => {
-        if(!loading)
-        {
-            getBookRating({
-                variables: {
-                    getBookRatingBookId: data.getAdvertisement.book.id
-                },
-                onCompleted(data) {
-                    setRating(data.getBookRating);
-                },
-            });
-        }
-    }, [loading]);
 
 
     if (loading || trendingLoading) return <>Loading...</>
@@ -36,26 +19,15 @@ function Test() {
         <div id={css["root"]}>
             <div className={css["section"]}>
                 <h1>Book Details</h1>
-                <Advert 
-                    id={router.query.id as string} 
-                    title={data.getAdvertisement.book.bookName} 
-                    author={data.getAdvertisement.book.authorName} 
-                    desc={data.getAdvertisement.book.description}
-                    price={data.getAdvertisement.price} 
-                    seller={data.getAdvertisement.seller.fullName} 
-                    sellerNo={data.getAdvertisement.seller.phoneNo}
-                    image={data.getAdvertisement.images[0]}
-                    bought = {data.userBoughtAdvertisement}    
-                    rating={rating}
-                    bookId = {data.getAdvertisement.book.id}
-                />
+                <Advert id={router.query.id as string} title={data.getAdvertisement.book.bookName} author={data.getAdvertisement.book.authorName} desc={data.getAdvertisement.book.description}
+                    price={data.getAdvertisement.price} seller={data.getAdvertisement.seller.fullName} image={data.getAdvertisement.images[0]}></Advert>
             </div>
 
             <div className={css["similar"]}>
                 <h1>More books</h1>
                 <div className={css["scroll"]}>
                     {trendingData?.getTrendingBooks.map(book => {
-                        return <BookCard key={book.bookName} title={book.bookName} description={book.description} imageURL={book.advertisements[0].images[0]} />
+                        return <BookCard key={book.bookName} title={book.bookName} description={book.description} imageURL={book.advertisements[0].images[0]}/>
                     })}
                 </div>
             </div>
--- src/components/advert/advert.tsx
+++ src/components/advert/advert.tsx
@@ -1,10 +1,9 @@
-import { AdvertisementType, useRateBookMutation, useTouchAdvertisementMutation } from '@/graphql/generated/generated';
+import { AdvertisementType, useTouchAdvertisementMutation } from '@/graphql/generated/generated';
 import Button from '../button/button';
 import {Input} from '../input/input';
 import css from './advert.module.css';
 import React from 'react';
 import { useRouter } from 'next/router';
-import { AiFillStar } from 'react-icons/ai';
 
 //there will be some redundant stuff after integration
 interface AdvertProps{
@@ -14,11 +13,7 @@ interface AdvertProps{
     desc:string
     price:number,
     seller:string,
-    sellerNo:string,
-    image: string,
-    bought: boolean
-    rating: number
-    bookId: string
+    image: string
 };
 
 function Advert(e:AdvertProps)
@@ -26,10 +21,8 @@ function Advert(e:AdvertProps)
     const router = useRouter();
 
     const touchPriceRef = React.useRef<HTMLInputElement>();
-    const ratingRef = React.useRef<HTMLInputElement>();
 
     const [touchAdvertisement] = useTouchAdvertisementMutation();
-    const [rateBook] = useRateBookMutation();
 
 
     const onRequest = async (e) => {
@@ -57,32 +50,12 @@ function Advert(e:AdvertProps)
         </div>
         <div className={css["right"]}>
             <h1>{e.title}</h1>
-            {e.bought ? <p className=' flex items-center text-2xl font-bold'>{e.rating} <AiFillStar color='yellow' size={"1.5em"}/></p> : ''}
             <h2>{e.author}</h2>
             <p>{e.desc}</p>
             <h1>&#8377; {e.price}</h1>
         <div className={css["tags"]}></div>
             <h2> Seller</h2>
             <p>{e.seller}</p>
-            {e.bought && <p>{e.sellerNo}</p>}
-            {
-                e.bought ?
-                <div className='flex flex-col justify-start'>
-                <Input reference={ratingRef} onChange={e => console.log(touchPriceRef.current.value)} />
-                <Button onClick={async (ev) => {
-                    ev.preventDefault();
-
-                    await rateBook({
-                        variables: {
-                            rateBookBookId: e.bookId,
-                            rating: Number(ratingRef.current.value)
-                        }
-                    });
-                    router.reload();
-                }} label='Rate' variant='elong'></Button> 
-            </div>
-            : <></>
-            }
         <Button onClick={() => router.push("/actions")} label="Buy Now" variant='elong'></Button>
         </div>
     </div>
